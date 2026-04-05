9dab5a3e41ef55a3bbe265a49a40a97587f8fbc6
Shukur Shaik <shukursk7@gmail.com>
2026-04-05 16:51:01 +0530

    Updated embedding logic to copy1

--- app/services/embedding.py
+++ app/services/embedding.py
@@ -2,5 +2,5 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embeddings():
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-mpnet-base-v2"
     )
