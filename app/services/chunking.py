3ba6f9a1069cca1e2252a7cc02c603d4432643bb
Shukur Shaik <shukursk7@gmail.com>
2026-04-05 21:24:28 +0530

    updated chunking size to copy1

--- app/services/chunking.py
+++ app/services/chunking.py
@@ -2,7 +2,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
+        chunk_size=600,
         chunk_overlap=50
     )
     return splitter.split_documents(documents)
